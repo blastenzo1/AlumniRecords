56ef1188bc20835514737ed66bb59e5f6233591f
geromejangj <geromeldolorfino@su.edu.ph>
2024-03-01 14:44:55 -0800

    edited log in

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,69 +9,46 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+        <link rel="stylesheet" href='{{ asset('app.css') }}'>
 
         {{-- Bootstrap --}}
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    </head>"
-    <body class="my-login-page d-flex align-items-center justify-content-center" style="min-height:100vh;min-width:100vw;">
-        <section class="h-100">
-            <div class="container h-100">
-                <div class="row justify-content-md-center h-100">
-                    <div class="card-wrapper">
-                        <div class="brand">
-                            <img src="img/logo.jpg" alt="logo">
+    </head>
+    {{-- <body class="my-login-page d-flex align-items-center justify-content-center" style="min-height:100vh;min-width:100vw;"> --}}
+        <body>
+        <section class="vh-100 gradient-custom">
+            <div class="container py-5 h-100">
+              <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col-12 col-md-8 col-lg-6 col-xl-5">
+                  <div class="card bg-light text-black" style="border-radius: 1rem;">
+                    <div class="card-body p-5 text-center">
+          
+                      <div class="mb-md-5 mt-md-4 pb-5">
+          
+                        <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                        <p class="text-black-50 mb-5">Please enter your login and password!</p>
+          
+                        <div class="form-outline form-white mb-4">
+                          <input type="email" id="typeEmailX" class="form-control form-control-lg" />
+                          <label class="form-label" for="typeEmailX">Username</label>
                         </div>
-                        <div class="card fat">
-                            <div class="card-body">
-                                <h4 class="card-title">Silliman Login</h4>
-                                <form method="POST" class="my-login-validation" novalidate="">
-                                    {{-- <div class="form-group">
-                                        <label for="email">E-Mail Address</label>
-                                        <input id="email" type="email" class="form-control" name="email" value="" required autofocus>
-                                        <div class="invalid-feedback">
-                                            Email is invalid
-                                        </div>
-                                    </div> --}}
-
-                                    
-
-                                    <div class="form-group">
-                                        <label for="password">Password
-                                            <a href="forgot.html" class="float-right">
-                                                Forgot Password?
-                                            </a>
-                                        </label>
-                                        <input id="password" type="password" class="form-control" name="password" required data-eye>
-                                        <div class="invalid-feedback">
-                                            Password is required
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group">
-                                        <div class="custom-checkbox custom-control">
-                                            <input type="checkbox" name="remember" id="remember" class="custom-control-input">
-                                            <label for="remember" class="custom-control-label">Remember Me</label>
-                                        </div>
-                                    </div>
-
-                                    <div class="form-group m-0">
-                                        <button type="submit" class="btn btn-primary btn-block">
-                                            Login
-                                        </button>
-                                    </div>
-                                    <div class="mt-4 text-center">
-                                        Don't have an account? <a href="register.html">Create One</a>
-                                    </div>
-                                </form>
-                            </div>
+          
+                        <div class="form-outline form-white mb-4">
+                          <input type="password" id="typePasswordX" class="form-control form-control-lg" />
+                          <label class="form-label" for="typePasswordX">Password</label>
                         </div>
-                        <div class="footer">
-                            Copyright &copy; 2017 &mdash; Your Company
-                        </div>
+          
+                        <button class="btn btn-outline-dark btn-lg px-5" type="submit">Login</button>
+          
+                      </div>
+          
                     </div>
+                  </div>
                 </div>
+              </div>
             </div>
-        </section>
+          </section>
+                                                                             
 
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+// module.exports = {
+//   content: ['./src/**/*.{html,js}'], 
+//   theme: {
+//       extend: {}
+//   }, 
+//   plugins: []
+// }
+
