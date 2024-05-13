26d59143b5b78e5c7981ef1fdee571d818167f64
geromejangj <geromeldolorfino@su.edu.ph>
2024-05-13 16:29:33 -0700

    Added record sorting and finished form label

---
+++ resources/views/Auth/finishedform.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Login</title>
+
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    <link rel="stylesheet" href='{{ asset('app.css') }}'>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.2.19/tailwind.min.css" rel="stylesheet">
+</head>
+<body>
+    <section class="bg-gradient-to-b from-red-900 from-70% to-bg-white to-30%">
+        <div class="min-h-screen flex flex-col justify-center items-center">
+            <main class="flex-1 flex flex-col w-full py-6 space-y-8">
+                <a href="/" class="flex-none flex justify-center sm:justify-start items-center px-6 cursor-pointer">
+                    <img src="{{ asset('Pics/alumniRec.png') }}" alt="Logo" class="mb-4 w-96">
+                </a>
+
+                <div class="flex-1 grid grid-cols-1 place-content-center justify-center items-center space-y-6p px-4">
+                    <div class="text-center text-white space-y-4">
+                        <header class="font-bold text-4xl">FORM SUBMITTED!</header>
+                        <p class="text-xl">Thhank you for filling up the form.</p>
+                    </div>
+                </div>
+            </main>
+
+            <footer class="w-full bg-red-900 text-right text-white p-4">
+                Silliman University Alumni Records.
+            </footer>
+        </div>
+    </section>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    {{-- <script src="js/my-login.js"></script> --}}
+</body>
+</html>
